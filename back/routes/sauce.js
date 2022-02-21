c553f8bfb51a515001f3e6c768b828e488be9125
Cinzia80 <cinziambea80@gmail.com>
2022-02-21 16:44:08 +0100

    env

--- back/routes/sauce.js
+++ back/routes/sauce.js
@@ -37,29 +37,6 @@ router.get('/', auth, sauceCtrl.readAllSauces);
 //-----------------//
 router.post('/:id/like', auth, sauceCtrl.likeDislike);
 //----------------//
-// Exports router ////----------------//
-// Import express //
-// Create router  //
-//----------------//
-const express = require('express');
-const router = express.Router();
-//----------------//
-// Import limiter //
-//----------------//
-const limiter = require('../middleware/limiter');
-//-------------------//
-// Import controller //
-//-------------------//
-const userCtrl = require('../controllers/user');
-//---------------//
-// Create routes //
-//---------------//
-router.post('/signup', userCtrl.signup);
-router.post('/login', limiter.loginLimiter, userCtrl.login);
-//----------------//
 // Exports router //
-//----------------//
-module.exports = router;
-
 //----------------//
 module.exports = router;
